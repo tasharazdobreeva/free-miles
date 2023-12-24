d22afc3ca9775fed7d3961c9532afc3aabc5cb58
natali.r <natasha-razdobreeva@mail.ru>
2023-12-24 20:42:21 +0300

    free miles решение3

--- freeMiles/src/Main.java
+++ freeMiles/src/Main.java
@@ -1,10 +1,8 @@
 public class Main {
     public static void main(String[] args) {
 
-        int ticket; // стоимость белета
-        ticket = 5_000;
-        int mile; // количество рублей для одной бонусной мили
-        mile = 20;
+        int ticket = 5_000;
+        int mile = 20;
 
         System.out.println("Количество бонусов за 1 билет:");
         System.out.println(ticket / mile);
